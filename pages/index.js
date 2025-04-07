99b7d39ce6959066302d30501a0846fef7ff4ded
dream-walker-ck <dreamwalkertp@gmail.com>
2025-04-08 07:46:29 +0900

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useEffect } from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
+import Dashboard from "../components/Dashboard";
+
+export const runtime = "edge";
+
+const Home = () => {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+  return (
+    <div style={{ display: "flex" }}>
+      <Sidebar />
+      <div style={{ flexGrow: 1 }}>
+        <Header />
+        <Dashboard />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
